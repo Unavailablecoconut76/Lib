f023a741bcccd4a40a2dd38b95301372bee21311
Unavailablecoconut76 <unavailablecoconut76@gmail.com>
2025-06-13 05:09:32 +0530

    deployment prep

--- back/database/db.js
+++ back/database/db.js
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
 
-export const connectDB=async()=>{
-    mongoose.connect(process.env.MONGO_URI,{
-        dbName:"sqlproj"
-    }).then(()=>{
-        console.log(`dtatbase connected..`)
-    }).catch(err=>{
-        console.log(`error conecting to database`,err)
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "sqlproj",
+      // Add these options for better connection handling
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000, // Increase timeout to 20s
+      socketTimeoutMS: 45000, // Increase socket timeout
+      family: 4 // Use IPv4, skip trying IPv6
     });
+    
+    console.log(`Database connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to database: ${error.message}`);
+    process.exit(1);
+  }
 };
