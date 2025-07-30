fdb0610cd0fba6cfa9e6d7474126edfa47ab5feb
Unavailablecoconut76 <unavailablecoconut76@gmail.com>
2025-07-30 18:55:45 +0530

    fixture-2

--- back/utils/sendToken.js
+++ back/utils/sendToken.js
@@ -5,7 +5,9 @@ export const sendToken =(user,statusCode,message,res)=>{
     cookie("token",token,{
         expires:new Date(
             Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000),
-        httpOnly:true
+        httpOnly:true,
+        secure:true,
+        sameSite:"none"
     }).json({
         success:true,
         user,
