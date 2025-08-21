34f9784dfb0b5371e95a478a55fa14655e47bb83
Unavailablecoconut76 <unavailablecoconut76@gmail.com>
2025-08-21 14:55:31 +0530

    avatar fix attempt 2

--- back/app.js
+++ back/app.js
@@ -26,7 +26,7 @@ app.use(express.json());//middlewares
 app.use(express.urlencoded({extended:true}));
 
 app.use(expressFileupload({
-    useTempfiles:true,
+    useTempFiles:true,
     tempFileDir:"/tmp/"
 }))
 
